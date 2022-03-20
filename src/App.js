10bc3a82acbe7c7ff74a704d95d7bc0cd8ef0043
Arunitkumar <m.arunitkumar@gmail.com>
2022-03-20 19:57:46 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TableDemo from "./Table";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			{/* Header with inline css */}
+			<h1
+				style={{
+					display: 'flex', justifyContent: 'center', padding: '15px',
+					border: '13px solid #b4f0b4', color: 'rgb(11, 167, 11)'
+				}}>
+				Material Table
+			</h1>
+			{/* Table component below header */}
+			<TableDemo />
+		</div>
+	)
 }
 
 export default App;
+
